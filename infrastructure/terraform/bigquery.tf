eb5e7e01d36970b50f317c121ea1aba6b04fe701
Sergei Lilichenko <slilichenko@google.com>
2024-10-27 20:58:18 -0700

    Added process watermark table and permissions for connections.

--- infrastructure/terraform/bigquery.tf
+++ infrastructure/terraform/bigquery.tf
@@ -18,6 +18,32 @@ resource "google_bigquery_dataset" "bus-stop-image-processing" {
   location      = var.bigquery_dataset_location
 }
 
+resource "google_bigquery_connection" "image_bucket_connection" {
+  connection_id = "image_bucket_connection"
+  project       = var.project_id
+  location      = var.bigquery_dataset_location
+  cloud_resource {}
+}
+
+resource "google_project_iam_member" "image_bucket_connection_permission" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = format("serviceAccount:%s", google_bigquery_connection.image_bucket_connection.cloud_resource[0].service_account_id)
+}
+
+resource "google_bigquery_table" "images" {
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.bus-stop-image-processing.dataset_id
+  table_id            = "images"
+  external_data_configuration {
+    connection_id       = google_bigquery_connection.image_bucket_connection.id
+    source_uris         = [format("gs://%s/images/*", google_storage_bucket.image_bucket.id)]
+    object_metadata     = "SIMPLE"
+    metadata_cache_mode = "MANUAL"
+    autodetect          = false
+  }
+}
+
 resource "google_bigquery_table" "reports" {
   deletion_protection = false
   dataset_id          = google_bigquery_dataset.bus-stop-image-processing.dataset_id
@@ -28,9 +54,76 @@ resource "google_bigquery_table" "reports" {
 }
 
 resource "google_bigquery_job" "reports_search_index" {
-  job_id = "reports_search_index"
+  job_id     = "reports_search_index"
+  depends_on = [google_bigquery_table.reports]
 
   query {
     query = "CREATE SEARCH INDEX IF NOT EXISTS reports_search_index ON ${google_bigquery_table.reports.table_id} (description)"
   }
 }
+
+resource "google_bigquery_table" "process_watermark" {
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.bus-stop-image-processing.dataset_id
+  table_id            = "process_watermark"
+  description         = "Table with a single row which contains the timestamp the data was last processed."
+  schema              = file("${path.module}/bigquery-schema/process_watermark.json")
+}
+
+resource "google_bigquery_job" "populate_process_watermark" {
+  job_id     = "populate_process_watermark"
+  depends_on = [google_bigquery_table.process_watermark]
+
+  query {
+    query = "INSERT INTO ${google_bigquery_table.process_watermark.table_id} VALUES (CURRENT_TIMESTAMP())"
+  }
+}
+
+#resource "google_bigquery_routine" "process_images_procedure" {
+#dataset_id = google_bigquery_dataset.bus-stop-image-processing.dataset_id
+#routine_id = "process_images"
+#routine_type = "PROCEDURE"
+#language = "SQL"
+#definition_body = <<END_OF_PROCEDURE
+#CREATE FUNCTION process_images
+#
+#BEGIN TRANSACTION;
+#DECLARE last_process_time TIMESTAMP;
+#DECLARE new_process_time TIMESTAMP;
+#SET last_process_time = (SELECT MAX(process_time) FROM ${google_bigquery_table.process_watermark.id});
+#SET new_process_time = CURRENT_TIMESTAMP();
+#
+#INSERT INTO ${google_bigquery_table.reports.id}
+#SELECT
+#  GENERATE_UUID() as report_id,
+#  uri,
+#  (SELECT value FROM UNNEST(metadata) WHERE name = 'stop_id') AS bus_stop_id,
+#  CAST (JSON_EXTRACT(ml_generate_text_llm_result, '$.cleanliness_level') AS INT64) AS cleanliness_level,
+#  JSON_EXTRACT(ml_generate_text_llm_result, '$.description') AS description,
+#FROM
+#  ML.GENERATE_TEXT(
+#   MODEL `multimodal.gemini_1_5_flash`,
+#   TABLE ${google_bigquery_table.images.id},
+#   STRUCT (
+#   '''
+#Analyse this image of a bus stop and provide the details below.
+#Return your answer in JSON format as follows. Do not include JSON decorators.
+#{
+#    "cleanliness_level": Integer, # Rate the cleanliness of this bus stop (From 0="Extremely dirty" to 10="Extremely clean"). Focus on literring and broken items.
+#    "description": String, # Details about what you see in the image in terms of cleanliness
+#}
+#   ''' AS PROMPT,
+#   0.2 AS temperature,
+#   TRUE AS FLATTEN_JSON_OUTPUT)
+#  )
+#WHERE content_type = "image/jpeg" AND updated > last_process_time;
+#
+#-- Update the process time watermark
+#UPDATE `multimodal.object_watermark`
+#SET process_time = new_process_time
+#WHERE TRUE;
+#
+#COMMIT TRANSACTION;
+#END
+#END_OF_PROCEDURE
+#}
--- infrastructure/terraform/service-account.tf
+++ infrastructure/terraform/service-account.tf
@@ -27,6 +27,14 @@ resource "google_project_iam_member" "data_processor_sa_bigquery_job_user" {
   role    = "roles/bigquery.jobUser"
 }
 
+resource "google_bigquery_connection_iam_member" "data_processor_sa_connection_access" {
+  project       = var.project_id
+  location      = var.bigquery_dataset_location
+  connection_id = google_bigquery_connection.image_bucket_connection.connection_id
+  role          = "roles/bigquery.connectionUser"
+  member        = local.member_data_processor_sa
+}
+
 resource "google_bigquery_table_iam_member" "data_processor_sa_bigquery_editor" {
   member = local.member_data_processor_sa
   role   = "roles/bigquery.dataEditor"
